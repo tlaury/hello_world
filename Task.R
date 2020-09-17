63d52ac66ebed235b27e8e841d6ccb8715cdd1b4
tlaury <68924947+tlaury@users.noreply.github.com>
2020-09-17 14:55:29 -0500

    Add files via upload
    Programming Assignment 2: Lexical Scoping

---
+++ Task.R
@@ -0,0 +1,31 @@
+
+matrixcache <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
+
+
